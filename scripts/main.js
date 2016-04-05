cc6cb317945991263025252d79a2d83a4b3c7ad4
Aleksandr Smirnov <al.smirnov01@yandex.ua>
2016-04-06 01:33:48 +0300

    Clean code

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,3 @@
-var tbl = document.getElementById("table");
-
-tbl.addEventListener("click", function(event){
-	if (event.target.className === 'col') {
-			var cols = document.getElementsByClassName('col');
-			var tds = document.getElementsByTagName('INPUT');
-
-			for (var i = 0; i < cols.length; i++) {
-				cols[i].classList.remove('selected_line');
-			}
-			for (var i = 0; i < tds.length; i++) {
-				tds[i].classList.remove('selected_td');
-			}
-
-			var thisColLetter = event.target.textContent;
-		 	event.target.classList.add('selected_line');
-		 	for(var i = 1; i <= 6; i++) {
-		 		document.getElementById(thisColLetter + i).classList.add('selected_td');
-		 	}
- 	}
-
- 	// $.ajax({
-  //       type: 'GET',
-  //       url: "data/json.php",
-  //       accept: 'application/json',
-  //       contentType: "application/json; charset=utf-8",
-  //       success: function (data) {
-  //           for (key in data) {
-  //           	alert(data[key]);
-  //           }
-  //       },
-  //       error: function () {
-  //           alert('Error');
-  //       }
-  //   });
-});
-
 function createTable() {
 	var count = 100;
 	var tbody = document.getElementById('tbody');
@@ -47,6 +10,7 @@ function createTable() {
 	for (var i = 1; i <= count; i++) {
 		tr = document.createElement('tr');
 		th = document.createElement('th');
+		// th.setAttribute('class','col')
 		div = document.createElement('div');
 
 		tr.appendChild(th).appendChild(div);
@@ -70,10 +34,46 @@ function createTable() {
 		tableHeadRow.appendChild(th.cloneNode(true));
 		div.innerHTML = i;
 	}
-
-	console.log(alphabet);
 	
 
 	tableHeadRow.appendChild(th.cloneNode(true));
 }
 createTable();
+
+// var tbl = document.getElementById("table");
+
+// tbl.addEventListener("click", function(event){
+// 	if (event.target.className === 'col') {
+// 			var cols = document.getElementsByClassName('col');
+// 			var tds = document.getElementsByTagName('INPUT');
+			
+
+// 			for (var i = 0; i < cols.length; i++) {
+// 				cols[i].classList.remove('selected_line');
+// 			}
+// 			for (var i = 0; i < tds.length; i++) {
+// 				tds[i].classList.remove('selected_td');
+// 			}
+
+// 			var thisColLetter = event.target.textContent;
+// 		 	event.target.classList.add('selected_line');
+// 		 	for(var i = 1; i <= 6; i++) {
+// 		 		document.getElementById(thisColLetter + i).classList.add('selected_td');
+// 		 	}
+//  	}
+
+ 	// $.ajax({
+  //       type: 'GET',
+  //       url: "data/json.php",
+  //       accept: 'application/json',
+  //       contentType: "application/json; charset=utf-8",
+  //       success: function (data) {
+  //           for (key in data) {
+  //           	alert(data[key]);
+  //           }
+  //       },
+  //       error: function () {
+  //           alert('Error');
+  //       }
+  //   });
+// });
