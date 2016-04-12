1792c3d596ed0f065f0672ee20ce010a8cd26933
Aleksandr Smirnov <al.smirnov01@yandex.ua>
2016-04-12 20:46:21 +0300

    Change sheets and get data from local storage

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ var tableHeadRow = document.getElementById('tableHeadRow');
 var tr, th, div, input, td;
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 var alphabetLength = alphabet.length;
+var active_sheet = document.getElementsByClassName('active_sheet');
+var filled_cells = document.getElementsByClassName('filled');
 
 function createTable() {
 
@@ -64,6 +66,7 @@ excel.addEventListener("click", function(event){
 
  	else if (event.target.tagName === 'SPAN') {
  		var count_sheet = 0;
+ 		td = document.getElementsByTagName('td');
  		for (var i = 0; i < sheets.length; i++) {
 			sheets[i].classList.remove('active_sheet');
 			count_sheet++
@@ -73,9 +76,35 @@ excel.addEventListener("click", function(event){
 				var sheetsContainer = document.querySelector('.sheets');
 				sheetsContainer.appendChild(sheet);
 				sheet.innerHTML = ('Sheet'+count_sheet);
-				sheet.classList.add('active_sheet');				
+				sheet.classList.add('active_sheet');
+				for (n = 0; n < filled_cells.length; n++) {
+					filled_cells[n].innerHTML =  null;
+					filled_cells[n].classList.remove('filled');
+				}			
 			}
+
 		event.target.classList.add('active_sheet');
+		for (i = 0; i < td.length; i++) {
+			td[i].innerHTML = null;
+			// td[i].classList.remove('filled');
+		}
+		getMyObject = JSON.parse(localStorage.getItem(active_sheet[0].innerHTML));
+		if (getMyObject != null) {
+			for (i = 0; i < getMyObject.length; i++) {
+				for (k = 0; k < td.length; k++) {
+					if (td[k].parentNode.rowIndex === getMyObject[i].row && td[k].cellIndex === getMyObject[i].cell) {
+						td[k].innerHTML = getMyObject[i].val;
+					}
+				}
+			}	
+		}
+
+		else {
+			for (z = 0; z < td.length; z++) {
+				td[z].innerHTML = null;
+			}
+		}
+
 		new_sheet.classList.remove('active_sheet');
  	}
 
@@ -90,23 +119,28 @@ excel.addEventListener("click", function(event){
  });
 
 excel.addEventListener("blur", function(event) {
-	if (event.target.value != '') {
-		event.target.parentNode.classList.add('filled');
-	}	
-	event.target.parentNode.innerHTML = input.value;
+	if (event.target.tagName === 'INPUT') {
+		if (event.target.value != '') {
+			event.target.parentNode.classList.add('filled');
+		}
 
-	var filled_cells = document.getElementsByClassName('filled');
-	var storage_memory = [];
-	var active_sheet = document.getElementsByClassName('active_sheet');
-	
-	for (i = 0; i < filled_cells.length; i++) {
-		var myObject = {};
-		myObject.row = filled_cells[i].parentNode.rowIndex;
-		myObject.cell = filled_cells[i].cellIndex;
-		myObject.val = filled_cells[i].innerHTML;
-		storage_memory.push(myObject);
+		event.target.parentNode.innerHTML = input.value;
+
+		var storage_memory = [];
+		
+		
+		for (i = 0; i < filled_cells.length; i++) {
+			if (filled_cells[i].innerHTML != '') {
+				var myObject = {};
+					myObject.row = filled_cells[i].parentNode.rowIndex;
+					myObject.cell = filled_cells[i].cellIndex;
+					myObject.val = filled_cells[i].innerHTML;
+					storage_memory.push(myObject);
+			}
+		}
+		localStorage.setItem(active_sheet[0].innerHTML, JSON.stringify(storage_memory));
 	}
-	localStorage.setItem(active_sheet[0].innerHTML, JSON.stringify(storage_memory));
+
 }, true);
 
 excel.addEventListener("contextmenu", function(event){
