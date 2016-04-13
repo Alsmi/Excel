6744618059766214cee6d5cf2eea91c4d9db27b9
Aleksandr Smirnov <al.smirnov01@yandex.ua>
2016-04-14 00:15:42 +0300

    Correct local storage data, selected rows, selected cells

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var count = 100;
 var tbody = document.getElementById('tbody');
 var tableHeadRow = document.getElementById('tableHeadRow');
-var tr, th, div, input, td;
+var tr, th, div, input;
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 var alphabetLength = alphabet.length;
 var active_sheet = document.getElementsByClassName('active_sheet');
@@ -45,53 +45,106 @@ createTable();
 
 var excel = document.getElementById('excel');
 var lines = document.getElementsByClassName('line');
-var tds = document.getElementsByTagName('INPUT');
+var selected_lines = document.getElementsByClassName('selected_line');
 var sheets = document.getElementsByTagName('SPAN');
 var new_sheet = document.getElementById('new_sheet');
+var td = document.getElementsByTagName('td');
+var selected_cells = document.getElementsByClassName('selected_td');
+var coordinate_td = document.getElementsByClassName('coordinate_td');
+
 
 excel.addEventListener("click", function(event){
 	
-	if (event.target.className === 'line') {	
-
+	if (event.target.className === 'line' || event.target.className === 'coordinate_td') {	
+		
 		for (var i = 0; i < lines.length; i++) {
-			lines[i].classList.remove('selected_line');
+			lines[i].classList.remove('selected_line');		
 		}
-		for (var i = 0; i < tds.length; i++) {
-			tds[i].classList.remove('selected_td');
-		}
-
+ 		
 	 	event.target.classList.add('selected_line');
- 	}
+	 	for (var i = 0; i < td.length; i++) {
+			td[i].classList.remove('selected_td');
+			if (event.target.parentNode.parentNode.rowIndex === td[i].parentNode.rowIndex || event.target.parentNode.cellIndex === td[i].cellIndex) {
+				td[i].classList.add('selected_td');
+			}
+			else if (event.target.innerHTML === '') {
+				for (var i = 0; i < td.length; i++) {
+					td[i].classList.add('selected_td');
+				}
+			}
+		}	
 
+		// if (!isNaN(+event.target.innerHTML) && event.target.innerHTML != '') {
+		// 	for (var n = 0; n < lines.length; n++) {
+		// 		if (isNaN(+lines[n].innerHTML) === true) {
+					
+		// 			lines[n].classList.add('coordinate_td');
+		// 		}
+		// 	}
+		// }
+		// else if (isNaN(+event.target.innerHTML)){
+		// 	for (var n = 0; n < lines.length; n++) {
+		// 		if (isNaN(+lines[n].innerHTML) === false && lines[n].innerHTML != '') {
+					
+		// 			lines[n].classList.add('coordinate_td');
+		// 		}
+		// 	}
+		// }
+		// else {
+		// 	for (var n = 0; n < lines.length; n++) {
+		// 		if (lines[n].innerHTML != '') {
+		// 			lines[n].classList.add('coordinate_td');
+		// 		}
+		// 	}
+		// 	for (var i = 0; i < td.length; i++) {
+		// 		td[i].classList.add('selected_td');
+		// 	}
+		// }
+
+	}
+
+	 // else if (event.target.className === 'line') {
+	 	// for (var i = 0; i < lines.length; i++) {
+ 		// 	lines[i].classList.remove('coordinate_td');
+ 		// }
+	 // }
 
  	else if (event.target.tagName === 'SPAN') {
  		var count_sheet = 0;
- 		td = document.getElementsByTagName('td');
+ 		
+ 		for (var i = 0; i < lines.length; i++) {
+ 			lines[i].classList.remove('coordinate_td');
+ 		}
+
  		for (var i = 0; i < sheets.length; i++) {
 			sheets[i].classList.remove('active_sheet');
 			count_sheet++
 		}
-			if (event.target.id === 'new_sheet' && count_sheet <= 20) {
-				var sheet = document.createElement('span');
-				var sheetsContainer = document.querySelector('.sheets');
-				sheetsContainer.appendChild(sheet);
-				sheet.innerHTML = ('Sheet'+count_sheet);
-				sheet.classList.add('active_sheet');
-				for (n = 0; n < filled_cells.length; n++) {
-					filled_cells[n].innerHTML =  null;
-					filled_cells[n].classList.remove('filled');
-				}			
-			}
-
 		event.target.classList.add('active_sheet');
-		for (i = 0; i < td.length; i++) {
+		for (var i = 0; i < td.length; i++) {
 			td[i].innerHTML = null;
-			// td[i].classList.remove('filled');
+			td[i].classList.remove('selected_td');
 		}
+		if (event.target.id === 'new_sheet' && count_sheet <= 20) {
+			var sheet = document.createElement('span');
+			var sheetsContainer = document.querySelector('.sheets');
+			sheetsContainer.appendChild(sheet);
+			sheet.innerHTML = ('Sheet'+count_sheet);
+			sheet.classList.add('active_sheet');
+			for (var n = 0; n < filled_cells.length; n++) {
+				filled_cells[n].innerHTML =  null;
+				filled_cells[n].classList.remove('filled');
+			}		
+		}
+		
+		for (var i = 0; i < selected_lines.length; i++) {
+			selected_lines[i].classList.remove('selected_line');
+		}
+
 		getMyObject = JSON.parse(localStorage.getItem(active_sheet[0].innerHTML));
 		if (getMyObject != null) {
-			for (i = 0; i < getMyObject.length; i++) {
-				for (k = 0; k < td.length; k++) {
+			for (var i = 0; i < getMyObject.length; i++) {
+				for (var k = 0; k < td.length; k++) {
 					if (td[k].parentNode.rowIndex === getMyObject[i].row && td[k].cellIndex === getMyObject[i].cell) {
 						td[k].innerHTML = getMyObject[i].val;
 					}
@@ -100,7 +153,7 @@ excel.addEventListener("click", function(event){
 		}
 
 		else {
-			for (z = 0; z < td.length; z++) {
+			for (var z = 0; z < td.length; z++) {
 				td[z].innerHTML = null;
 			}
 		}
@@ -115,7 +168,15 @@ excel.addEventListener("click", function(event){
  			event.target.innerHTML = '';
  			event.target.appendChild(input).focus();
  		}	
+ 		for (var i = 0; i < selected_lines.length; i++) {
+				selected_lines[i].classList.remove('selected_line');
+			}
+		for (var i = 0; i < td.length; i++) {
+			td[i].classList.remove('selected_td');
+		}
  	}
+
+ 
  });
 
 excel.addEventListener("blur", function(event) {
@@ -126,19 +187,22 @@ excel.addEventListener("blur", function(event) {
 
 		event.target.parentNode.innerHTML = input.value;
 
+		
 		var storage_memory = [];
 		
-		
-		for (i = 0; i < filled_cells.length; i++) {
-			if (filled_cells[i].innerHTML != '') {
-				var myObject = {};
-					myObject.row = filled_cells[i].parentNode.rowIndex;
-					myObject.cell = filled_cells[i].cellIndex;
-					myObject.val = filled_cells[i].innerHTML;
-					storage_memory.push(myObject);
-			}
+		for (var i = 0; i < filled_cells.length; i++) {			
+				if (filled_cells[i].innerHTML != '') {
+					var myObject = {};
+						myObject.row = filled_cells[i].parentNode.rowIndex;
+						myObject.cell = filled_cells[i].cellIndex;
+						myObject.val = filled_cells[i].innerHTML;
+					
+						storage_memory.push(myObject);
+						
+				}				
+				localStorage.setItem(active_sheet[0].innerHTML, JSON.stringify(storage_memory));
 		}
-		localStorage.setItem(active_sheet[0].innerHTML, JSON.stringify(storage_memory));
+
 	}
 
 }, true);
